341dcea2b2816782be9ae99cacd9561e3a6ddce2
mitom2 <mitom2@op.pl>
2024-09-05 01:19:53 +0200

    Insertions implementations

--- src/Code/Code.cpp
+++ src/Code/Code.cpp
@@ -5,6 +5,18 @@ void szl::Code::insert(CodeLine line, std::size_t position)
     code.insert(code.begin() + position, line);
 }
 
+void szl::Code::insert(CodeLine line)
+{
+    insert(line, ++currentPosition);
+}
+
+void szl::Code::insert(std::string codeText)
+{
+    szl::CodeLine codeLine = code[currentPosition];
+    codeLine.contents = codeText;
+    insert(codeLine, ++currentPosition);
+}
+
 szl::CodeLine &szl::Code::operator[](std::size_t position)
 {
     return code[position];
