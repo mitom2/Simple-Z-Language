0d88a30c6adea392660e846acf7c975d867a60ad
mitom2 <mitom2@op.pl>
2025-02-04 22:57:51 +0100

    Console program

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include "Compiler/Compiler.hpp"
+#include "SZLException/SZLException.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    szl::compile("def.szl", "def.oi");
+    std::string in, out;
+    for (int i = 0; i + 1 < argc; i++)
+    {
+        if (argv[i] == "-i")
+            in = argv[i + 1];
+        else if (argv[i] == "-o")
+            out = argv[i + 1];
+    }
+    if (!in.length() || !out.length())
+    {
+        std::cout << "Too few arguments. Usage: szl -i [input file] -o [output file]\n";
+        return 64;
+    }
+    try
+    {
+        szl::compile(in, out);
+    }
+    catch (const szl::SZLException &e)
+    {
+        std::cout << e.what() << '\nProgram was not compiled.\n';
+        return 65;
+    }
     return 0;
 }
